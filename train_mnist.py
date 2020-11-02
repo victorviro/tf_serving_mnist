27464f380945c80ad393ef8778d2c7b0d6bc828d
victorviro <victor_sanchez91@hotmail.es>
2020-11-02 20:14:38 +0100

    Added script for training mnist fashion dataset, for loadinf trained model. Added gitignore

---
+++ train_mnist.py
@@ -0,0 +1,42 @@
+
+
+import os
+
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.mnist.load_data()
+X_train_full = X_train_full[..., np.newaxis].astype(np.float32) / 255.
+X_test = X_test[..., np.newaxis].astype(np.float32) / 255.
+X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_new = X_test[:3]
+
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28, 1]),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(lr=1e-2),
+              metrics=["accuracy"])
+history = model.fit(X_train, y_train, epochs=3, validation_data=(X_valid, y_valid))
+
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print('\nTest accuracy: {}'.format(test_acc))
+
+
+model_version = "0001"
+model_name = "my_mnist_model"
+model_path = os.path.join(model_name, model_version)
+print('Path where the model will be stored: {}\n'.format(model_path))
+
+tf.saved_model.save(model, model_path)
