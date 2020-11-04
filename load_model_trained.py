3bd5e5be6a4593fb16656386ccee468e6fcd838a
victorviro <victor_sanchez91@hotmail.es>
2020-11-04 21:25:16 +0100

    Extended load model trained script to make a prediction

--- load_model_trained.py
+++ load_model_trained.py
@@ -1,4 +1,17 @@
+import os
+
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
+cwd = os.getcwd()
+model_path = f'{cwd}/my_mnist_model/0001'
 model = keras.models.load_model(model_path)
+
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.mnist.load_data()
+X_train_full = X_train_full[..., np.newaxis].astype(np.float32) / 255.
+X_test = X_test[..., np.newaxis].astype(np.float32) / 255.
+
+X_new = X_test[:3]
+y_pred = model.predict(tf.constant(X_new, dtype=tf.float32))
